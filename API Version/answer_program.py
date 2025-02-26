fd20554f58921f3754ded207b9b09e189f850d1c
VonEquinox <VonCrystalOrchid@outlook.com>
2025-02-26 14:31:55 +0800

    change model name

--- API Version/answer_program.py
+++ API Version/answer_program.py
@@ -3,13 +3,13 @@ import LoveGemini
 
 #models:Grok2 Grok3 Grok3-Thinking Gemini-Thinking Gemini-2Pro
 def answer_question(question, model):
-    if model == "Grok2":
+    if model == "Grok-2":
         return FuckGrok.ask_grok2(question)
-    elif model == "Grok3":
+    elif model == "Grok-3":
         return FuckGrok.ask_grok3(question)
-    elif model == "Grok3-Thinking":
+    elif model == "Grok-3-Thinking":
         return FuckGrok.ask_grok3_thinking(question)
     elif model == "Gemini-Thinking":
         return LoveGemini.ask_gemini_thinking(question)
-    elif model == "Gemini-2Pro":
+    elif model == "Gemini-2-Pro":
         return LoveGemini.ask_gemini_2pro(question)
